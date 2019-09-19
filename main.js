b51c9624b4c3d590eb55302093da20881c61a28c
manyagoyal15 <43989106+manyagoyal15@users.noreply.github.com>
2019-09-19 21:13:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,141 @@
+function show(){
+    var x=document.getElementById("d1");
+    var y=document.getElementById("1");
+        if(x.style.display=="none")
+            {
+                x.style.display="block";
+                y.value="Close";
+            }
+        else{
+            x.style.display="none";
+            y.value="View";
+        }
+    document.getElementById("d2").style.display="none";
+    document.getElementById("d3").style.display="none";
+    document.getElementById("d4").style.display="none";
+    document.getElementById("d5").style.display="none";
+    document.getElementById("d6").style.display="none";
+    document.getElementById("2").value="View";
+    document.getElementById("3").value="View";
+    document.getElementById("4").value="View";
+    document.getElementById("5").value="View";
+    document.getElementById("6").value="View";
+    }
+    function show1(){
+    var x=document.getElementById("d2");
+    var y=document.getElementById("2");
+        if(x.style.display=="none")
+            {
+                x.style.display="block";
+                y.value="Close";
+            }
+        else{
+            x.style.display="none";
+            y.value="View";
+        }
+    document.getElementById("d1").style.display="none";
+    document.getElementById("d3").style.display="none";
+    document.getElementById("d4").style.display="none";
+    document.getElementById("d5").style.display="none";
+    document.getElementById("d6").style.display="none";
+    document.getElementById("1").value="View";
+    document.getElementById("3").value="View";
+    document.getElementById("4").value="View";
+    document.getElementById("5").value="View";
+    document.getElementById("6").value="View";
+    }
+    function show2(){
+    var x=document.getElementById("d3");
+    var y=document.getElementById("3");
+        if(x.style.display=="none")
+            {
+                x.style.display="block";
+                y.value="Close";
+            }
+        else{
+            x.style.display="none";
+            y.value="View";
+        }
+    document.getElementById("d1").style.display="none";
+    document.getElementById("d2").style.display="none";
+    document.getElementById("d4").style.display="none";
+    document.getElementById("d5").style.display="none";
+    document.getElementById("d6").style.display="none";
+    document.getElementById("1").value="View";
+    document.getElementById("2").value="View";
+    document.getElementById("4").value="View";
+    document.getElementById("5").value="View";
+    document.getElementById("6").value="View";
+    }
+    function show3(){
+   var x=document.getElementById("d4");
+    var y=document.getElementById("4");
+        if(x.style.display=="none")
+            {
+                x.style.display="block";
+                y.value="Close";
+            }
+        else{
+            x.style.display="none";
+            y.value="View";
+        }
+    document.getElementById("d1").style.display="none";
+    document.getElementById("d2").style.display="none";
+    document.getElementById("d3").style.display="none";
+    document.getElementById("d5").style.display="none";
+    document.getElementById("d6").style.display="none";
+    document.getElementById("1").value="View";
+    document.getElementById("2").value="View";
+    document.getElementById("3").value="View";
+    document.getElementById("5").value="View";
+    document.getElementById("6").value="View";
+    }
+    function show4(){
+   var x=document.getElementById("d5");
+    var y=document.getElementById("5");
+        if(x.style.display=="none")
+            {
+                x.style.display="block";
+                y.value="Close";
+            }
+        else{
+            x.style.display="none";
+            y.value="View";
+        }
+    document.getElementById("d1").style.display="none";
+    document.getElementById("d2").style.display="none";
+    document.getElementById("d3").style.display="none";
+    document.getElementById("d4").style.display="none";
+    document.getElementById("d6").style.display="none";
+    document.getElementById("1").value="View";
+    document.getElementById("2").value="View";
+    document.getElementById("3").value="View";
+    document.getElementById("4").value="View";
+    document.getElementById("6").value="View";
+    }
+    function show5(){
+   var x=document.getElementById("d6");
+   var y=document.getElementById("6");
+        if(x.style.display=="none")
+            {
+                x.style.display="block";
+                y.value="Close";
+            }
+        else{
+            x.style.display="none";
+            y.value="View";
+        }
+    document.getElementById("d1").style.display="none";
+    document.getElementById("d2").style.display="none";
+    document.getElementById("d3").style.display="none";
+    document.getElementById("d4").style.display="none";
+    document.getElementById("d5").style.display="none";
+    document.getElementById("1").value="View";
+    document.getElementById("2").value="View";
+    document.getElementById("3").value="View";
+    document.getElementById("4").value="View";
+    document.getElementById("5").value="View";
+    }
 //Hamburger Menu Toggle start
 $(".navbar-nav li a").click(function(event) {
     // check if window is small enough so dropdown is created
